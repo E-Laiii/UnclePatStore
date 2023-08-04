6f9f6b9dfde70cf818698d2587252c79bbd51493
Elai <eliorakhong@gmail.com>
2023-08-05 04:41:56 +0800

    fix: moved vue files to correct directory

---
+++ uncle_pat_frontend/src/components/AddItem.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+      <h2>Add Item</h2>
+      <form @submit.prevent="addItem">
+        <input v-model="newItem.name" type="text" placeholder="Item Name" required>
+        <input v-model="newItem.price" type="number" placeholder="Item Price" required>
+        <button type="submit">Add Item</button>
+      </form>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        newItem: {
+          name: '',
+          price: null,
+        },
+      };
+    },
+    methods: {
+      addItem() {
+        this.$emit('add-item', { ...this.newItem });
+        this.newItem = { name: '', price: null };
+      },
+    },
+  };
+  </script>
+  

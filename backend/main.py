4449eeec43dc47031a76cd703fcde4275d465cdc
Elai <eliorakhong@gmail.com>
2023-08-05 03:36:57 +0800

    feat: Logging info template

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,12 @@
 from fastapi import FastAPI
+import logging
+from models import Item
+
 app = FastAPI()
+logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(message)s")
+
+
+store = []
 
 @app.get("/")
 async def root():

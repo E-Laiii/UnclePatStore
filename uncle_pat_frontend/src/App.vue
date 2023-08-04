0750435bf88c9a759ef02f2ac6afe3ef575a2b1e
Elai <eliorakhong@gmail.com>
2023-08-05 04:42:51 +0800

    fix: updated App.vue file to fetch components

--- uncle_pat_frontend/src/App.vue
+++ uncle_pat_frontend/src/App.vue
@@ -1,26 +1,52 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img alt="Vue logo" src="./assets/logo1.png">
+  <div id="app">
+    <h1>Basic Store</h1>
+    <item-list :items="items" />
+    <add-item @add-item="handleAddItem" />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios';
+import ItemList from './components/ItemList.vue';
+import AddItem from './components/AddItem.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    ItemList,
+    AddItem,
+  },
+  data() {
+    return {
+      items: [],
+    };
+  },
+  methods: {
+    async fetchItems() {
+      try {
+        const response = await axios.get('http://127.0.0.1:8000/items/');
+        this.items = response.data;
+      } catch (error) {
+        console.error('Error fetching items:', error);
+      }
+    },
+    async handleAddItem(item) {
+      try {
+        const response = await axios.post('http://127.0.0.1:8000/items/', item);
+        this.items.push(response.data.item);
+      } catch (error) {
+        console.error('Error adding item:', error);
+      }
+    },
+  },
+  created() {
+    this.fetchItems();
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+/* Add any custom styles here */
 </style>

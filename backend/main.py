64efc2a8ecd8cf9816fbb7b49f0c82f38fc48bbe
Elai <eliorakhong@gmail.com>
2023-08-05 03:39:45 +0800

    feat: added get and post calls for item in store

--- backend/main.py
+++ backend/main.py
@@ -11,3 +11,23 @@ store = []
 @app.get("/")
 async def root():
     return {"message": "hello! Backend fastapi is working."}
+
+@app.post("/items/")
+def add_item(item: Item):
+    # Append the item to the store list
+    store.append(item)
+
+    # Log the event
+    logging.info(f"Item added: {item}")
+
+    return {"message": "Item added successfully", "item": item}
+
+@app.get("/items/")
+def get_all_items():
+
+    # Log the event
+    logging.info(f"List of items fetched successfully")
+
+    # Fetch list of items
+    return store
+

649fb049f3bad378ae3938df090e47da8a947183
Elai <eliorakhong@gmail.com>
2023-08-05 04:25:37 +0800

    feat: Base skeleton of frontend (vue.js CLI)

---
+++ uncle_pat_frontend/src/ItemList.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+      <h2>Items in Store</h2>
+      <ul>
+        <li v-for="item in items" :key="item.id">
+          {{ item.name }} - ${{ item.price }}
+        </li>
+      </ul>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      items: Array,
+    },
+  };
+  </script>
+  

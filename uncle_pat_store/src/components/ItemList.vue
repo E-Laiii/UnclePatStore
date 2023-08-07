2137c5f919a13453d54261aa4211232c1deac6eb
Elai <eliorakhong@gmail.com>
2023-08-08 00:22:13 +0800

    fix: improved ui design of items in store for users and add to cart button

--- uncle_pat_store/src/components/ItemList.vue
+++ uncle_pat_store/src/components/ItemList.vue
@@ -1,26 +1,37 @@
 <template>
-    <div>
-      <h2>Items in Store</h2>
-      <ul>
-        <li v-for="item in items" :key="item.id">
-          {{ item.name }} - ${{ item.price }}
-        </li>
-      </ul>
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    props: {
-      items: Array,
+  <div>
+    <h2>Items in Store</h2>
+    <q-list class="q-mb-md">
+      <q-item v-for="item in items" :key="item.id">
+        <q-item-section>
+          <div class="text-h6">{{ item.name }}</div>
+          <div class="q-caption text-grey">{{ item.description }}</div>
+        </q-item-section>
+        <q-item-section side>
+          <div class="text-h6 text-weight-medium">${{ item.price }}</div>
+        </q-item-section>
+        <q-item-section side>
+          <q-btn @click="addToCart(item)" color="primary" label="Add to Cart" />
+        </q-item-section>
+      </q-item>
+    </q-list>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    items: Array,
+  },
+  methods: {
+    addToCart(item) {
+      // You can implement your logic for adding the item to the cart here
+      console.log("Adding item to cart:", item);
     },
-  };
-  </script>
+  },
+};
+</script>
 
 <style>
-
-ul {
-  list-style: none;
-}
+/* You can add custom styles here if needed */
 </style>
-  

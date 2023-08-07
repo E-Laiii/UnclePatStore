79e547ab20690af098a55c5bfd5b0cb04deb0c14
Elai <eliorakhong@gmail.com>
2023-08-08 00:22:50 +0800

    feat: Added authentication for admin before entering admin page

--- uncle_pat_store/src/views/AdminPage.vue
+++ uncle_pat_store/src/views/AdminPage.vue
@@ -1,37 +1,69 @@
 <template>
-    <div class="q-pa-md">
-        <h1>Admin Page</h1>
-        <p>This is where you can change/alter your store items.</p>
 
-        <add-item @add-item="handleAddItem" />
-    </div>
-    <div>
+  <div>
+    <!-- Admin Authentication Modal -->
+    <q-dialog v-model="showModal">
+      <q-card>
+        <q-card-section>
+          <q-input v-model="username" label="Username" />
+          <q-input v-model="password" label="Password" type="password" />
+        </q-card-section>
 
+        <q-card-actions align="right">
+          <q-btn @click="authenticate" label="Login" color="primary" />
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
+
+    <!-- Admin Page Content -->
+    <div v-if="loggedIn">
+      <h1>Admin Page</h1>
+      <p>This is where you can change/alter your store items.</p>
+      <!-- Rest of the admin page content goes here -->
+      <add-item @add-item="handleAddItem" />
     </div>
+  </div>
+  <div></div>
 </template>
 
 <script>
 // import axios from 'axios';
-import AddItem from '../components/AdminMethods.vue';
+import AddItem from "../components/AdminMethods.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    AddItem
-},
+    AddItem,
+  },
   data() {
     return {
+      showModal: true,
+      username: "",
+      password: "",
+      loggedIn: false,
       items: [],
     };
   },
   methods: {
-    goToAdminPage(){
-      this.$router.push({ path: '/admin-page' })
+    goToAdminPage() {
+      this.$router.push({ path: "/admin-page" });
     },
+    authenticate() {
+      // Perform authentication logic here (e.g., check credentials against server)
+      // For simplicity, let's just assume a hardcoded admin username and password
+      const adminUsername = "adminpat";
+      const adminPassword = "adminpat";
+
+      if (this.username === adminUsername && this.password === adminPassword) {
+        // Successful authentication
+        this.loggedIn = true;
+        this.showModal = false;
+      } else {
+        // Invalid credentials, show an error message or handle as needed
+        alert("Invalid credentials. Please try again.");
+      }
+    },
+
   },
-
 };
-
-
-
 </script>
